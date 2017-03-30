9beb367f1ae24827b81941e7b3e09fb52fc9b1cc
itichausali2 <itichausali@rediffmail.com>
2017-03-30 16:01:24 +0530

    README

---
+++ src/main/java/com/AutomatePaytm/App.java
@@ -0,0 +1,13 @@
+package com.AutomatePaytm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
