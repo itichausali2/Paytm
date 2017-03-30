15375bb8e22ec44ac175f0f70410ec0d6ec02b98
My PC <itichausali@gmail.com>
2017-03-30 23:05:05 +0530

    CHANG IN COMMIT

--- src/test/java/com/AutomatePaytm/Paytm.java
+++ src/test/java/com/AutomatePaytm/Paytm.java
@@ -1,23 +1,26 @@
 package com.AutomatePaytm;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class Paytm {
-	WebDriver driver;
-	@BeforeTest
-	public void launchBrowser() {
-		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"//Drivers//geckodriver.exe");
-		driver = new FirefoxDriver();
-		driver.get("https://paytm.com/");
-	}
-	@Test
-	public void login() {
-		
-	}
-	
 
+public class Paytm {
+	
+	
+		WebDriver driver;
+		@BeforeTest
+		public void launchBrowser() {
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"//Drivers//geckodriver.exe");
+			driver = new FirefoxDriver();
+			driver.get("https://paytm.com/");
+		}
+		@Test
+		public void login() {
+			
+		
+		}
+
+	
 
 }
---
+++ src/test/java/com/AutomatePaytm/Paytmone.java
@@ -0,0 +1,22 @@
+
+package com.AutomatePaytm;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Paytmone {
+	WebDriver driver;
+	@BeforeTest
+	public void launchBrowser() {
+		System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir")+"//Drivers//geckodriver.exe");
+		driver = new FirefoxDriver();
+		driver.get("https://paytm.com/");
+	}
+	@Test
+	public void login() {
+		
+	
+	}
+
+}
